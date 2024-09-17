9d17a46ce181719ab667e984a1fd7f931e9dd831
ZhiWei Tan <longtzw@proton.me>
2024-09-17 10:44:00 +0800

    delete custom keymap

--- init.lua
+++ init.lua
@@ -163,7 +163,6 @@ vim.opt.scrolloff = 10
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('i', 'jk', '<Esc><C-k>', { desc = 'Exit Insert Mode' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
