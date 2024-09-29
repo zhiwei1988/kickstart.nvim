9622030bc59956a8fa893535342f746a34a58927
ZhiWei Tan <longtzw@proton.me>
2024-09-29 13:31:15 +0800

    Support Custom Options

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- [[Custom Setting Options]]
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- 使用空格替换Tab
+vim.opt.expandtab = true
+
 return {}
