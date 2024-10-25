362b80a309d784f7e05cb43b422d46bb021bc181
ZhiWei Tan <longtzw@proton.me>
2024-10-26 00:27:29 +0800

    support gitsigns

---
+++ lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,44 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  event = { 'BufReadPre', 'BufNewFile' }, -- 在打开或创建文件时加载
+  opts = {
+    -- 显示行修改状态
+    signs = {
+      add = { text = '│' },
+      change = { text = '│' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+
+    current_line_blame_opts = {
+      delay = 1000,
+    },
+
+    -- 开启当前行 blame
+    current_line_blame = true,
+
+    -- blame 信息格式
+    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+    -- blame 信息显示延迟（毫秒）
+
+    -- 设置预览窗口的位置
+    preview_config = {
+      border = 'rounded',
+      style = 'minimal',
+      relative = 'cursor',
+      row = 0,
+      col = 1,
+    },
+  },
+  keys = {
+    -- 键位映射
+    { '<leader>hb', ':Gitsigns blame_line<CR>', desc = 'Git blame line' },
+    { '<leader>hp', ':Gitsigns preview_hunk<CR>', desc = 'Git preview hunk' },
+    { '<leader>hs', ':Gitsigns stage_hunk<CR>', desc = 'Git stage hunk' },
+    { '<leader>hr', ':Gitsigns reset_hunk<CR>', desc = 'Git reset hunk' },
+    { '<leader>hu', ':Gitsigns undo_stage_hunk<CR>', desc = 'Git undo stage hunk' },
+    { ']c', ':Gitsigns next_hunk<CR>', desc = 'Next git hunk' },
+    { '[c', ':Gitsigns prev_hunk<CR>', desc = 'Previous git hunk' },
+  },
+}
