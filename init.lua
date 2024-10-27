c0f628eb517af4406f6c93974ca0566156a2aee2
ZhiWei Tan <longtzw@proton.me>
2024-10-27 10:49:23 +0800

    support lsp hover

--- init.lua
+++ init.lua
@@ -550,6 +550,8 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>dd', vim.lsp.buf.hover, '[D]isplay [D]efinition')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
