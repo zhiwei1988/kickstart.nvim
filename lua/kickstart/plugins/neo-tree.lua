88bb78954ded82aa9ddd1f22391b8b1954221fc3
ZhiWei Tan <zhiweix1988@gmail.com>
2026-02-19 21:36:35 +0800

    [feature]neo-tree支持文件名复制

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -18,6 +18,36 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          -- 绑定 Y 键触发自定义函数
+          ["Y"] = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local results = {
+              ["文件名"] = filename,
+              ["绝对路径"] = path,
+              ["相对路径"] = modify(path, ":."),
+              ["家目录相对路径"] = modify(path, ":~"),
+            }
+
+            local options = vim.tbl_keys(results)
+            table.sort(options)
+            
+            vim.ui.select(options, {
+              prompt = "选择要复制的内容:",
+              format_item = function(item)
+                return ("%s: %s"):format(item, results[item])
+              end,
+            }, function(choice)
+              local result = results[choice]
+              if result then
+                vim.fn.setreg("+", result) -- 复制到系统剪贴板
+                vim.notify("已复制: " .. result)
+              end
+            end)
+          end,
         },
       },
     },
