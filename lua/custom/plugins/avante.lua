5121da361298b69c14ecf5eef7ad735a3b877c63
ZhiWei Tan <zhiweix1988@gmail.com>
2026-02-19 21:27:52 +0800

    [feature]新增gemini模型

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -5,14 +5,30 @@ return {
   version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
-    provider = 'gemini',
+    -- mode = "legacy",
+    mode = "agentic",
+    provider = 'gemini_pro',
+    auto_suggestions_provider = 'gemini_flash',
+    behaviour = {
+      auto_suggestions = true,
+    },
     providers = {
-      gemini = {
+      gemini_pro = {
+        __inherited_from = 'gemini',
         endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
-        model = 'gemini-3.0-pro',
+        model = 'gemini-3-pro-preview',
         timeout = 30000,
         temperature = 0,
         max_tokens = 4096,
+        api_key_name = 'GEMINI_API_KEY',
+      },
+      gemini_flash = {
+        __inherited_from = 'gemini',
+        endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
+        model = 'gemini-3-flash-preview',
+        timeout = 30000,
+        temperature = 0,
+        max_tokens = 2048,
         api_key_name = 'GEMINI_API_KEY',
       },
     },
