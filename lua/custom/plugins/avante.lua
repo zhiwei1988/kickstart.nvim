01d8dab59fb0ac895adab038f77a410af2604329
ZhiWei Tan <zhiweix1988@gmail.com>
2026-02-19 19:14:58 +0800

    [feature]修改默认使用的gemini模型

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -9,7 +9,7 @@ return {
     providers = {
       gemini = {
         endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
-        model = 'gemini-2.5-pro',
+        model = 'gemini-3.0-pro',
         timeout = 30000,
         temperature = 0,
         max_tokens = 4096,
